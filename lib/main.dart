edd6129325d74bf8545c9b2f6722bc36db56f569
RosarioCaltabianoCPG <rosario.caltabiano@cloudpathology.eu>
2025-09-09 16:12:32 +0200

    First  commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int soundNumber) {
+    AudioPlayer().play(AssetSource("note$soundNumber.wav"));
+  }
+
+  Expanded buildKey (int keyNumber, Color color) {
+    return Expanded(
+      child: TextButton(
+          onPressed: () {
+            playSound(keyNumber);
+          },
+          child: Text(""),
+          style: TextButton.styleFrom(
+            backgroundColor: color,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.zero, // niente arrotondamenti
+            ),
+          )),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
-        ),
-      ),
+            child: Column(
+          children: [
+            buildKey(1, Colors.red),
+            buildKey(2, Colors.orange),
+            buildKey(3, Colors.yellow),
+            buildKey(4, Colors.green),
+            buildKey(5, Colors.teal),
+            buildKey(6, Colors.blue),
+          ],
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+        )),
+      backgroundColor: Colors.black,),
     );
   }
 }
